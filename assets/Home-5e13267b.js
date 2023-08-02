bb58f0126e0237197dd0fb4f78758eebeccbba60
Hanna <anna_kon@bigmir.net>
2023-08-02 19:51:31 +0300

    Updates

---
+++ assets/Home-5e13267b.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-56a373a2.js";const r=()=>e.jsx("div",{children:"Home"});export{r as default};
---
+++ assets/Warehouses-5da23424.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-56a373a2.js";const r=()=>s.jsx("div",{children:"Warehouses"});export{r as default};
