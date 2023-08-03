5512e0a5659ccc5d699458ef8632c334c0168059
Hanna <anna_kon@bigmir.net>
2023-08-03 18:01:51 +0300

    Updates

---
+++ assets/Home-a8145749.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-458e43e7.js";const r=()=>e.jsx("div",{children:"Home"});export{r as default};
--- assets/Warehouses-5da23424.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-56a373a2.js";const r=()=>s.jsx("div",{children:"Warehouses"});export{r as default};
