c9ccdbe17ee7d923f13fd5c8abd43ef547df5436
Hanna <anna_kon@bigmir.net>
2023-08-10 21:04:23 +0300

    deploy corrections

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { App } from "./components/App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { store, persistor } from "./redux/store.js";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
@@ -30,7 +31,7 @@ const theme = createTheme({
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/parcel-tracking-service/">
+      <HashRouter basename="/parcel-tracking-service">
         <PersistGate loading={null} persistor={persistor}>
           <ThemeProvider theme={theme}>
             <App />
@@ -48,7 +49,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
             theme="colored"
           />
         </PersistGate>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
